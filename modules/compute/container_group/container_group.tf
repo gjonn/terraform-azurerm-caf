9c8aa462993e1e3df68a8a459d1f7237198b24e8
Gjon Gojcevic <gjonn@hotmail.com>
2024-02-21 14:18:33 -0500

    Update container_group.tf

--- modules/compute/container_group/container_group.tf
+++ modules/compute/container_group/container_group.tf
@@ -33,7 +33,7 @@ resource "azurerm_container_group" "acg" {
   tags                = merge(local.tags, try(var.settings.tags, null))
   ip_address_type     = try(var.settings.ip_address_type, "Public")
   restart_policy      = try(var.settings.restart_policy, "Always")
-  subnet_id = coalesce(
+  subnet_ids = coalesce(
   try(ip_configuration.value.subnet_id, null),
   try(var.remote_objects.networking[ip_configuration.value.lz_key][ip_configuration.value.vnet_key].subnets[ip_configuration.value.subnet_key].id, null),
   try(var.remote_objects.networking[var.client_config.landingzone_key][ip_configuration.value.vnet_key].subnets[ip_configuration.value.subnet_key].id, null),
